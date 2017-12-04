8da0fdba630e24b1de2250c2531e6507958ecc59
Aaron Anderson <aaron.anderson.a7x@gmail.com>
2017-12-04 16:17:31 +0000

    Trying to get Heroku to work

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    socket.on('connection', function(conn){
-        io.emit('A user has Connected...');
-    });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
 });
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
